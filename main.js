f4953fa19528e06d47f0b8dd575501f0953ec31b
Zsetta <134535531+Zsetta@users.noreply.github.com>
2026-08-27 21:34:53 -0300

    .

---
+++ main.js
@@ -0,0 +1,58 @@
+const lista = document.querySelector(".lista");
+const input = document.querySelector(".input");
+const enviar = document.querySelector(".enviar");
+
+const recorrer = () => {
+    const deleteB = document.querySelectorAll(".deleteB");
+    for(const del of deleteB)
+    {
+        del.addEventListener("click",(e)=>{
+            e.target.parentElement.remove();
+        });
+    }
+    for(const li of lista.children)
+    {
+    li.addEventListener("dragstart",(e)=>{
+        setTimeout(()=>{
+            e.target.classList.add("moving");
+            
+        },0);
+    });
+    li.addEventListener("dragover",(e)=>{
+        e.preventDefault();
+        let datos = e.target.getBoundingClientRect();
+        let l = document.querySelector(".moving");
+        if(e.clientY >= datos.y + datos.height/2)
+        { 
+            li.parentElement.insertBefore(l,e.target.nextElementSibling);
+        }
+        else
+        {
+            li.parentElement.insertBefore(l,e.target);
+        }
+    });
+    li.addEventListener("dragend",(e)=>{
+        e.target.classList.remove("moving");
+    });
+    }
+    
+}
+
+enviar.addEventListener("click",(e)=>{
+    if(input.value.length > 0)
+    {
+        let li = document.createElement("LI");
+        let deleteBtn = document.createElement("BUTTON");
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.classList.add("deleteB");
+        li.innerHTML = input.value;
+        li.appendChild(deleteBtn);
+        li.draggable = "true";
+        lista.appendChild(li);
+        input.value="";
+        recorrer();
+    }
+});
+
+
+recorrer();
